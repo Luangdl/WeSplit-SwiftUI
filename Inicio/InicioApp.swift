55cec168a9fb7c1b4fe171b25e53a34a6a9e0671
Luangdl <luam_gdl@hotmail.com>
2024-01-18 19:16:02 -0300

    first commit

---
+++ Inicio/InicioApp.swift
@@ -0,0 +1,17 @@
+//
+//  InicioApp.swift
+//  Inicio
+//
+//  Created by Luan.Lima on 13/01/24.
+//
+
+import SwiftUI
+
+@main
+struct InicioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
